b89ebe4ff041d12b7000b8365587e7cad496447e
Saman Abbasi <samyar1992@gmail.com>
2023-12-30 03:47:11 +0330

    add logo

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,17 +1,21 @@
 import { FC, ReactElement } from "react";
-
+import Styles from "./Layout.module.scss";
+import logo from "../../assets/snaptrip.png"
 type Props = {
     children: ReactElement
 }
 
-const Layout: FC<Props> = ({children}) => {
+const Layout: FC<Props> = ({ children }) => {
   return (
-    <div>
-        <div>Sticky Navbar with SnappTrip Icon</div>
-        <div>
-          {children}
-        </div>
+    <div className={Styles.layout}>
+      <div className={Styles.navbar}>
+        <a href="https://flighttrip.netlify.app/" className={Styles.logo}>
+          <img src={logo} alt="logo" className={Styles.img} />
+        </a>
+      </div>
+      <div className={Styles.content}>{children}</div>
     </div>
-  )
-}
-export default Layout; 
+  );
+};
+
+export default Layout;
